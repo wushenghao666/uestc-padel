ec4d0e2bfadc8d509a43126ff5ec50207333a699
Shenghao Wu <wushenghaop@qq.com>
2026-06-12 15:15:31 +0800

    Tighten ranking player column

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "uestc-padel-v6";
+const CACHE_NAME = "uestc-padel-v7";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.json", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
